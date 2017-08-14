fd521d9511045e936c0fcabbc2e8d9057ad07099
zhy <find404@foxmail.com>
2017-08-14 12:21:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 				
         //主题部分。
         var ThreeAjax = {
-						Interactive : [],
-						Send	: 	(function(){
-									var Url		  = (this.Interactive[0].Url==undefined) 		? window.location.pathname : this.Interactive[0].Url;
-									var Operation = (this.Interactive[0].Operation==undefined)  ? 'post'				   : this.Interactive[0].Operation;
-									$[Operation](Url, this.Interactive[0],function(data) {
-										if(data){
-											if(data.ret==100){
-												ThreeAjax.Interactive[1](data);
-											}else{
-												if(ThreeAjax.Interactive[3]==undefined){
-														alert('数据有误');
-													}else{
-														ThreeAjax.Interactive[3];
-												}
-											}
-										}else{
-											if(ThreeAjax.Interactive[3]==undefined){
-                            alert('网络有误！');
-												}else{
-													ThreeAjax.Interactive[3];
-											}
-										}
-									});
-							})
+		Interactive : [],
+		Send	: (function(){
+			     var Url		= (this.Interactive[0].Url==undefined) 		? window.location.pathname : this.Interactive[0].Url;
+			     var Operation   = (this.Interactive[0].Operation==undefined)    ? 'post'				   : this.Interactive[0].Operation;
+				$[Operation](Url, this.Interactive[0],function(data) {
+				if(data){
+					if(data.ret==100){
+						ThreeAjax.Interactive[1](data);
+					}else{
+						if(ThreeAjax.Interactive[3]==undefined){
+								alert('数据有误');
+						}else{
+								ThreeAjax.Interactive[3];
+						}
+					}
+				}else{
+					if(ThreeAjax.Interactive[3]==undefined){
+						alert('网络有误！');
+					}else{
+						ThreeAjax.Interactive[3];
+					}
+				}
+				});
+				})
 				};
         
         
         //调用    eg:
         //数据准备层
         	ThreeAjax.Interactive.push({
-										'category_id': '<?php echo $_GET['category_id'];?>',
-										'goods_series_id': '<?php echo $_GET['goods_series_id'];?>',
-										'goods_type': 3,
-										'order': 'goods_id',
-										'page':1,
-										'page_size':20,
-										'desc':'desc',
-										'Url' : '/Goods/goodsData'
+			'order': 'goods_id',
+			'page':1,
+			'page_size':20,
+			'desc':'desc',
+			'Url' : '/Goods/goodsData'
 						});
             
          //成功页面调用层
